affc2511c2d28062fec1934b73b7c8bb2964dc84
Surya <singhsurya1012@gmail.com>
2020-08-03 22:53:52 +0530

    updated logic for handling text messages

--- src/main/java/com/chatterbox/chat/CustomSocketHandler.java
+++ src/main/java/com/chatterbox/chat/CustomSocketHandler.java
@@ -1,5 +1,6 @@
 package com.chatterbox.chat;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,6 +19,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class CustomSocketHandler extends TextWebSocketHandler {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(CustomSocketHandler.class);
+    public static final String secretKey = "$%^&";
 
     List<WebSocketSession> users = new CopyOnWriteArrayList<>();
     Map<String, String> usernameMap = new ConcurrentHashMap<>();
@@ -38,8 +41,31 @@ public class CustomSocketHandler extends TextWebSocketHandler {
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session, message);
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        //Invoked when a message arrives
+        if (message.getPayload().contains(secretKey)) {
+            String name = message.getPayload().replace(secretKey, "");
+            usernameMap.put(session.getId(), name);
+        } else {
+
+            //Sending users message
+            users.stream()
+                    .filter(WebSocketSession::isOpen)
+                    .forEach(webSocketSession -> {
+                        try {
+                            //Sending actual message to all users
+                            webSocketSession.sendMessage(message);
+
+                            //Updating list of people online
+                            String onlineUsers = "@#$:" + StringUtils.join(usernameMap.values(), ",");
+                            webSocketSession.sendMessage(new TextMessage(onlineUsers));
+                        } catch (IOException e) {
+                            LOGGER.info("Error occured while sending message");
+                        }
+                    });
+
+
+        }
     }
 
 
